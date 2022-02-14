1dd1798c15cbf851138d15d63cca0b3883e7bb52
Angela De Pasquale <angeladepasquale_@libero.it>
2022-02-14 14:50:42 +0100

    Aggiunte le preview chat, anche se da sistemare

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const vue = new Vue (
                             date: '10/01/2020 15:30:55',
                             text: 'Hai portato a spasso il cane?',
                             status: 'sent'
-                        },
+                        }/*,
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Ricordati di dargli da mangiare',
@@ -24,7 +24,7 @@ const vue = new Vue (
                             date: '10/01/2020 16:15:22',
                             text: 'Tutto fatto!',
                             status: 'received'
-                        }
+                        }*/
                     ],
                 },
                 {
@@ -36,7 +36,7 @@ const vue = new Vue (
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
                         status: 'sent'
-                        },
+                        },/*
                         {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
@@ -46,7 +46,7 @@ const vue = new Vue (
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
-                        }
+                        }*/
                     ],
                 },
                 {
@@ -58,7 +58,7 @@ const vue = new Vue (
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
                         status: 'received'
-                        },
+                        },/*
                         {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
@@ -68,8 +68,9 @@ const vue = new Vue (
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
                         status: 'received'
-                        }
+                        }*/
                     ],
+                   
                 },
                 {
                     name: 'Luisa',
@@ -80,15 +81,15 @@ const vue = new Vue (
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
-                        },
+                        },/*
                         {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
                         status: 'received'
-                        }
+                        }*/
                     ],
                 },
-            ]                
+            ]             
         }
 
     }
