2c51cd24eeaf1ea0e8a3ef815fe214f8b56785e0
Angela De Pasquale <angeladepasquale_@libero.it>
2022-02-15 13:13:48 +0100

    Aggiustata chat a sinistra e visualizzati messaggi di una chat

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const vue = new Vue (
         el: '#archive-chat',
         data: {
 
+            
+
             contacts: [
                 {
                     name: 'Michele',
@@ -14,7 +16,7 @@ const vue = new Vue (
                             date: '10/01/2020 15:30:55',
                             text: 'Hai portato a spasso il cane?',
                             status: 'sent'
-                        }/*,
+                        },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Ricordati di dargli da mangiare',
@@ -24,7 +26,9 @@ const vue = new Vue (
                             date: '10/01/2020 16:15:22',
                             text: 'Tutto fatto!',
                             status: 'received'
-                        }*/
+                        }
+                        
+                        
                     ],
                 },
                 {
@@ -36,7 +40,7 @@ const vue = new Vue (
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
                         status: 'sent'
-                        },/*
+                        },
                         {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
@@ -46,7 +50,7 @@ const vue = new Vue (
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
-                        }*/
+                        }
                     ],
                 },
                 {
@@ -58,7 +62,7 @@ const vue = new Vue (
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
                         status: 'received'
-                        },/*
+                        },
                         {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
@@ -68,7 +72,7 @@ const vue = new Vue (
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
                         status: 'received'
-                        }*/
+                        }
                     ],
                    
                 },
@@ -81,16 +85,16 @@ const vue = new Vue (
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
-                        },/*
+                        },
                         {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
                         status: 'received'
-                        }*/
+                        }
                     ],
                 },
-            ]             
+
+            ],
+            
         }
-        
-    }
-)
+})
